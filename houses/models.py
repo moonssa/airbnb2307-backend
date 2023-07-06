05ea9f613c60d22fac3e7a9ec4c83fb5407cbd73
moonk <mkjoehsb@gmail.com>
2023-07-06 21:06:59 +0900

    customize admin pannel with list_editable...

--- houses/models.py
+++ houses/models.py
@@ -7,7 +7,11 @@ class House(models.Model):
     """model definition for houses"""
 
     name=models.CharField(max_length=140)
-    price=models.PositiveBigIntegerField()
+    price_per_night=models.PositiveBigIntegerField(verbose_name="Price")
     description=models.TextField()
     address=models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True, verbose_name="Pets Allowed", 
+                                       help_text="Does this house allow pets?",)
 
+    def __str__(self):
+        return self.name
