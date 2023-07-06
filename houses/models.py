b62b086f3d3d4c783b158d91d24d814496ed276a
moonk <mkjoehsb@gmail.com>
2023-07-07 00:46:51 +0900

    initialize user model

--- houses/models.py
+++ houses/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+
 class House(models.Model):
 
     """model definition for houses"""
 
-    name=models.CharField(max_length=140)
-    price_per_night=models.PositiveBigIntegerField(verbose_name="Price")
-    description=models.TextField()
-    address=models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True, verbose_name="Pets Allowed", 
-                                       help_text="Does this house allow pets?",)
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveBigIntegerField(verbose_name="Price")
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True, verbose_name="Pets Allowed", help_text="Does this house allow pets?")
 
     def __str__(self):
         return self.name
