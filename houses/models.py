54adb90fae0976ee4db5bda63ed7664dfaf8750d
moonk <mkjoehsb@gmail.com>
2023-07-07 10:19:29 +0900

    relationship with foreign key

--- houses/models.py
+++ houses/models.py
@@ -13,5 +13,7 @@ class House(models.Model):
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True, verbose_name="Pets Allowed", help_text="Does this house allow pets?")
 
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
